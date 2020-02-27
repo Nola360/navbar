1e356ae5ba4699f94b1861083ab317d63c6cc48b
nola360 <adaramolajr@gmail.com>
2020-02-26 19:07:48 -0500

    added nav bar with simple javascript console log

---
+++ app.js
@@ -0,0 +1,116 @@
+const home = document.querySelector('.box1');
+const about = document.querySelector('.box2');
+const media = document.querySelector('.box3');
+const contact = document.querySelector('.box4');
+
+//BUTTONS
+home.addEventListener('click', function () {
+  console.log('home');
+  console.log(home);
+});
+about.addEventListener('click', function () {
+  console.log('about');
+  console.log(about);
+});
+media.addEventListener('click', function () {
+  console.log('media');
+  console.log(media)
+});
+contact.addEventListener('click', function () {
+  console.log('contact');
+  console.log(contact)
+});
+
+
+
+// const keys = document.querySelectorAll('.box');
+// const keys = document.getElementsByClassName('box1', 'box2', 'box3', 'box4');
+// keys.forEach(box => box.addEventListener('click', keypad));
+
+// function keypad() {
+//   switch (keys) {
+//     case '.box box1':
+//       console.log('media');
+//       break;
+
+
+
+//   }
+
+
+
+
+
+
+//   let home = document.querySelector('.box1');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // switch () {
+  //   case 'box box1':
+  //     console.log('home');
+  //     break;
+  //   case 'box2':
+  //     console.log('about');
+  //     break;
+  //   case 'box3':
+  //     console.log('media');
+  //     break;
+  //   case 'box4':
+  //     console.log('contact');
+  //     break;
+  //   default:
+  //     console.log('not working');
+  // }
+
+
+// const navBtn = document.querySelector('.box').addEventListener('click', function () {
+//   console.log('test');
+// });
+
+
+// const navBtn = document.querySelector('.box').addEventListener('click', function (e) {
+//   let button = e.target;
+//   if (button.className === 'box box1') {
+//     console.log('home')
+//   }
+// })
+
+
+// BUTTONS
+
+
+
